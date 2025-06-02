df63ff304961e7256bdaa5cda376a9dc59bb197c
Vedha Konduru <VedhaKonduru08@gmail.com>
2025-06-02 19:19:48 +0530

    (a+b)^2

---
+++ myformula1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    
+    
+    scanf("%d,%d",&a,&b);
+    int result = (a+b)*(a+b);
+    
+    
+    
+    
+    printf("the answer is %d\n",result);
+    
+    return 0;
+    
+    
+}
