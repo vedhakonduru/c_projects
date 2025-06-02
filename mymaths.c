455d28c686fdfb2f3cf424fbaa4ca97328e9332f
Vedha Konduru <VedhaKonduru08@gmail.com>
2025-06-02 19:10:33 +0530

    added addition programme

---
+++ mymaths.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int a = 10; // Declare and initialize integer variable 'a'
+    int b = 20; // Declare and initialize integer variable 'b'
+    int c = a + b; // Perform addition and store in 'c'
+
+    // Print the result using printf
+    printf("Addition of %d and %d is %d\n", a, b, c); // Added a newline character for better formatting
+}
