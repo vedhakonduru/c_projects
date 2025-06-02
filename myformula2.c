2c2db59f4a20a25d380d360ee54309f04f27d2a0
Vedha Konduru <VedhaKonduru08@gmail.com>
2025-06-02 19:28:21 +0530

    assending order programme

---
+++ myformula2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    scanf("%d,%d,%d,%d,%d", &a, &b, &c, &d, &e);
+
+    printf("all are not in neither assending order nor decending order");
+
+    return 0;
+}
