b403c544cd80c77a75aebafef6d5e4b8d8cc5572
Vedha <vedhakonduru08@gmail.com>
2025-07-23 17:42:54 +0530

    assignment operator

---
+++ PRS8.c
@@ -0,0 +1,15 @@
+/*example for assignment operator*/
+#include <stdio.h>
+int main()
+{
+    int a= 10;
+    printf("find value of a: %d\n", a);
+    a=30;
+    printf("find value of a: %d\n", a);
+    a=50;
+    printf("find value of a: %d\n", a);
+    a=70;
+    printf("find value of a: %d\n", a);
+
+    return 0;
+}
