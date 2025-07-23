62c2922583a917c348fa2043e48112cd38b4bc98
Vedha <vedhakonduru08@gmail.com>
2025-07-23 18:03:41 +0530

    pre decrementation

---
+++ PRS13.c
@@ -0,0 +1,19 @@
+/*pre decrimentation*/
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    printf("Initial value of a: %d\n", a);
+    
+    // Pre-decrement
+    int b = --a;
+    printf("Value of b after pre-decrementing a: %d\n", b);
+    printf("Value of a after pre-decrementing: %d\n", a);
+    
+    // Pre-increment
+    int c = ++a;
+    printf("Value of c after pre-incrementing a: %d\n", c);
+    printf("Value of a after pre-incrementing: %d\n", a);
+    
+    return 0;
+}
