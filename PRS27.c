e68dd37d06cb822bf16363b19bc27f3db91719be
Vedha <vedhakonduru08@gmail.com>
2025-07-28 18:25:39 +0530

    sum of N natural numbers

---
+++ PRS27.c
@@ -0,0 +1,21 @@
+/*sum of N natural numbers with help of loops*/
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    printf("The sum of the first %d natural numbers is: %d\n", n, sum);
+
+    return 0;
+}
